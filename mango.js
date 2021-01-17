497d17095af7caea5e353ba8e9ecd850bb249721
Drishti2007 <72103713+Drishti2007@users.noreply.github.com>
2021-01-17 15:38:06 +0000

    Add files via upload

---
+++ mango.js
@@ -0,0 +1,30 @@
+class Mango {
+    constructor(x, y,){
+      var options = {
+        'restitution':0,
+        'friction':1.0,
+        'isStatic':true
+    }
+      this.image = loadImage("mangoed.png");
+      this.x = x;
+      this.y = y;
+      this.r = 30;
+      
+      this.body = Bodies.circle(this.x,this.y,15,options);
+    World.add(world,this.body)
+  
+
+    }
+  display()
+  {
+   var pos =this.body.position;
+   
+    push();
+    translate(pos.x, pos.y);
+   
+    strokeWeight(4);
+    imageMode(CENTER);
+    image(this.image,0,0,this.r*2,this.r*2);
+    pop();
+  }
+}
